88c309aafe0a3f486fe8bfc0f94190d0f27c04c4
Lucas <lucasferron06@gmail.com>
2024-09-12 23:03:08 -0300

    Fundamentos001

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-let mensagem = "hello world";
-console.log(mensagem) //hello world
+// arrays
+let metas = ["Lucas", "olá"]
+
+console.log(metas[1] + " " + metas[0]);
+
+// objetos
+
+let metasOBJ = {
+    value: 'ler um livro por mês',
+    checked: false
+}
+console.log(metasOBJ.value)
+
+// Arrow function
+//const CriarMeta = () => {}
+
+
+//function
+//function CriarMeta() {}
